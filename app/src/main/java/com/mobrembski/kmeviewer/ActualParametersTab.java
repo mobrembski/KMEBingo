6af67b6fa35172766465bca348a83751134b145a
Michał Obrembski <byku@byku.com.pl>
2015-01-02 17:36:00 +0100

    Added rest of actual parameters. Still in very simple view.

--- app/src/main/java/com/mobrembski/kmeviewer/ActualParametersTab.java
+++ app/src/main/java/com/mobrembski/kmeviewer/ActualParametersTab.java
@@ -1,6 +1,7 @@
 package com.mobrembski.kmeviewer;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.widget.TextView;
 
 import com.mobrembski.kmeviewer.SerialFrames.ActualFrame;
@@ -20,9 +21,67 @@ public class ActualParametersTab extends KMEViewerTab {
             main.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    TextView tv = (TextView) myView.findViewById(R.id.textView2);
                     KMEDataActual dtn = KMEDataActual.GetDataFromByteArray(frame);
+                    TextView tv = (TextView) myView.findViewById(R.id.TPSValue);
                     tv.setText(String.valueOf(dtn.TPS));
+                    tv = (TextView) myView.findViewById(R.id.LambdaValue);
+                    tv.setText(String.valueOf(dtn.Lambda));
+                    if (dtn.LambdaColor==1) {
+                        tv.setTextColor(Color.GREEN);
+                    } else if (dtn.LambdaColor==4) {
+                        tv.setTextColor(Color.RED);
+                    } else {
+                        tv.setTextColor(Color.YELLOW);
+                    }
+                    tv = (TextView) myView.findViewById(R.id.ActuatorValue);
+                    tv.setText(String.valueOf(dtn.Actuator)+" PWA:"+String.valueOf(dtn.PWA));
+                    tv = (TextView) myView.findViewById(R.id.TemperatureValue);
+                    tv.setText(String.valueOf(dtn.ActualTemp));
+                    tv = (TextView) myView.findViewById(R.id.RPMOKValue);
+                    if(dtn.RPOK) {
+                        tv.setText("RPM OK!");
+                        tv.setTextColor(Color.GREEN);
+                    }
+                    else {
+                        tv.setText("RPM TOO LOW!");
+                        tv.setTextColor(Color.RED);
+                    }
+                    tv = (TextView) myView.findViewById(R.id.TemperatureOKValue);
+                    if(dtn.TemperatureOK) {
+                        tv.setText("TEMP OK!");
+                        tv.setTextColor(Color.GREEN);
+                    }
+                    else {
+                        tv.setText("TEMP TOO LOW!");
+                        tv.setTextColor(Color.RED);
+                    }
+                    tv = (TextView) myView.findViewById(R.id.RunningFuelTypeValue);
+                    if(dtn.WorkingOnGas) {
+                        tv.setText("Working on GAS!");
+                        tv.setTextColor(Color.GREEN);
+                    }
+                    else {
+                        tv.setText("Working on Benzin!");
+                        tv.setTextColor(Color.RED);
+                    }
+                    tv = (TextView) myView.findViewById(R.id.CutOffActiveValue);
+                    if(dtn.CutOffActivated) {
+                        tv.setText("CUT OFF ACTIVE!");
+                        tv.setTextColor(Color.YELLOW);
+                    }
+                    else {
+                        tv.setText("");
+                        tv.setTextColor(Color.RED);
+                    }
+                    tv = (TextView) myView.findViewById(R.id.IgnitionValue);
+                    if(dtn.Ignition) {
+                        tv.setText("Ignition ON!");
+                        tv.setTextColor(Color.YELLOW);
+                    }
+                    else {
+                        tv.setText("Ignition OFF");
+                        tv.setTextColor(Color.RED);
+                    }
                 }
             });
     }
--- app/src/main/java/com/mobrembski/kmeviewer/KMEDataActual.java
+++ app/src/main/java/com/mobrembski/kmeviewer/KMEDataActual.java
@@ -1,16 +1,36 @@
 package com.mobrembski.kmeviewer;
 
 public class KMEDataActual {
-    public float TPS;
-    public int actuator;
-    public int actualTemp;
+    public int TPS;
+    public int Lambda;
+    public int Actuator;
+    public int ActualTemp;
+    public int PWA;
+    public boolean WorkingOnGas;
+    public boolean CutOffActivated;
+    public boolean TemperatureOK;
+    public boolean RPOK;
+    public boolean Ignition;
+    public int LambdaColor;
+    public int TPSColor;
 
     public static KMEDataActual GetDataFromByteArray(int[] array) {
         KMEDataActual dataActual = new KMEDataActual();
         if (array.length > 0) {
             dataActual.TPS = 0 + array[1];
-            dataActual.actuator = 0 + array[3];
-            dataActual.actualTemp = 0 + array[9];
+            dataActual.Lambda = 0 + array[2];
+            dataActual.Actuator = 0 + array[3];
+            dataActual.PWA = 0 + array[4];
+
+            dataActual.WorkingOnGas = (array[8] & 1) == 1;
+            dataActual.CutOffActivated = (array[8] & 8) == 8;
+            dataActual.TemperatureOK = (array[8] & 128) == 128;
+            dataActual.Ignition = (array[7] & 8) == 8;
+            dataActual.LambdaColor = (array[7] & 7);
+            dataActual.TPSColor = (array[7] & 240);
+
+            dataActual.RPOK = (array[8] & 64) == 64;
+            dataActual.ActualTemp = 0 + array[9];
         }
         return dataActual;
     }
