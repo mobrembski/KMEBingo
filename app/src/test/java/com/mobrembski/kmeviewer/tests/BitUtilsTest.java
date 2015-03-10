f2a1b664dd663ba79c12fee4a3b0d5827b2a0348
Michał Obrembski <m.obrembski@samsung.com>
2015-03-10 15:47:07 +0100

    Skel of UnitTests
    Providing UnitTest will give more comfort in
    changes in Settings in near future.
    
    This commit modifies project to use Gradle
    in version 1.1 and jUnit4.
    
    This commit has been made using tutorial from:
    http://tools.android.com/tech-docs/unit-testing-support

---
+++ app/src/test/java/com/mobrembski/kmeviewer/tests/BitUtilsTest.java
@@ -0,0 +1,70 @@
+package com.mobrembski.kmeviewer.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BitUtilsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testBitIsSet() throws Exception {
+
+    }
+
+    @Test
+    public void testSetBit() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMaskedBytes() throws Exception {
+
+    }
+
+    @Test
+    public void testGetVoltage() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTemperature() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRPM() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRawDate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRawRunningCounter() throws Exception {
+
+    }
+
+    @Test
+    public void testPackFrame() throws Exception {
+
+    }
+
+    @Test
+    public void testPackFrame1() throws Exception {
+
+    }
+}
---
+++ app/src/test/java/com/mobrembski/kmeviewer/tests/KMEDataSettingsTest.java
@@ -0,0 +1,58 @@
+package com.mobrembski.kmeviewer.tests;
+
+import com.mobrembski.kmeviewer.SerialFrames.KMEDataSettings;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KMEDataSettingsTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLevelSensor() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLevelSensorRaw() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLevelSensor() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLambdaEmulationType() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLambdaEmulationType() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEconomyMode() throws Exception {
+
+    }
+
+    @Test
+    public void testSetEconomyMode() throws Exception {
+
+    }
+}
