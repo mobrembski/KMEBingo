0552fabfa9615921f18f4cc74b15b1a97b1ad0da
Michał Obrembski <byku@byku.com.pl>
2015-05-09 14:54:29 +0200

    Added version information in AboutDialog

--- app/src/main/java/com/mobrembski/kmeviewer/AboutDialog.java
+++ app/src/main/java/com/mobrembski/kmeviewer/AboutDialog.java
@@ -2,10 +2,12 @@ package com.mobrembski.kmeviewer;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /*
 * This class is made only for compatibility.
@@ -24,6 +26,16 @@ public class AboutDialog extends Dialog {
         setContentView(R.layout.about_dialog);
         final ImageView logo = (ImageView)findViewById(R.id.about_logo);
         final Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.about_logo);
+        final TextView version = (TextView)findViewById(R.id.about_version);
+        try {
+            PackageInfo pInfo = getContext().getPackageManager().getPackageInfo(
+                    getContext().getPackageName(), 0);
+            String versionStr = pInfo.versionName;
+            version.setText("v" + versionStr);
+        } catch (Exception e) {
+            version.setText("");
+        }
+
         logo.startAnimation(anim);
     }
 }
